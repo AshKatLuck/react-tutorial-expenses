798c7db55caac2f0bc761b226dfa04c5beed4e5c
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-01-09 20:19:19 -0800

    revert to multiple states for storing changes in the form fields

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -2,33 +2,35 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  //   const [enteredTitle, setEnteredTitle] = useState("");
-  //   const [enteredAnount, setEnteredAmount] = useState("");
-  //   const [enteredDate, setEnteredDate] = useState("");
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAnount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
   const titleChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
-    });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+    setEnteredTitle(event.target.value);
     console.log(event.target.value);
   };
   const amountChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
-    });
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+    setEnteredAmount(event.target.value);
     console.log(event.target.value);
   };
   const dateChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      entereddate: event.target.value,
-    });
+    // setUserInput({
+    //   ...userInput,
+    //   entereddate: event.target.value,
+    // });
+    setEnteredDate(event.target.value);
     console.log(event.target.value);
   };
 
