3e7b19fba6f744ef76e8aa264ad352a8e562fa75
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-01-10 20:56:42 -0800

    Move the list logic to another file expenseList.js with its own css Add a powerpoint slide with the structure of the App till now

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,54 +1,30 @@
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import React, { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filterByYear, setFilterByYear] = useState("2020");
 
   const changeFilterYearHandler = (enteredFilterYear) => {
     setFilterByYear(enteredFilterYear);
-    // console.log("from Expense.js");
-    // console.log(filterByYear, enteredFilterYear);
   };
+  const expensesForSelectedYear = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === filterByYear
+  );
 
-  // const expenses = props.expenses;
   return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filterByYear}
-        onChangeFilterYear={changeFilterYearHandler}
-      />
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filterByYear}
+          onChangeFilterYear={changeFilterYearHandler}
         />
-      ))}
-      {/* <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      /> */}
-    </Card>
+
+        <ExpensesList expenses={expensesForSelectedYear} />
+      </Card>
+    </li>
   );
 };
-
 export default Expenses;
